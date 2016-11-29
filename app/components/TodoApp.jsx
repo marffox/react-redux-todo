9ae4b1063dad4fe1f7870d5cc178012e49fb5385
Rafael <marffox@gmail.com>
2016-11-29 22:04:25 +0100

    Create AddTodo component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,36 @@
 var React = require('react');
+var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
 
 var TodoApp = React.createClass({
+	getInitialState: function() {
+		return {
+			todos: [
+				{
+					id: 1,
+					text: 'Pasear al perro'
+				}, {
+					id: 2,
+					text: 'Comprar la cena'
+				}, {
+					id: 3,
+					text: 'Limpiar la casa'
+				}, {
+					id: 4,
+					text: 'Lavar la moto'
+				}
+			]
+		}
+	},
+	handleAddTodo: function(text) {
+		alert('New todo: ' + text);
+	},
 	render: function() {
+		var {todos} = this.state;
 		return (
 			<div>
-				TodoApp.jsx
+				<TodoList todos={todos}/>
+				<AddTodo onAddTodo={this.handleAddTodo}/>
 			</div>
 		);
 	}	
---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jquery');
+
+var TodoList = require('TodoList');
+var Todo = require('Todo');
+
+describe('TodoList', () => {
+	it('should exist', () => {
+		expect(TodoList).toExist();
+	});
+
+	it('should render one Todo component for each todo item', () => {
+		var todos = [{
+			id: 1,
+			text: 'Do something'
+		},{
+			id: 2,
+			text: 'Send email'
+		}];
+		var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+		var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);//cuantos componentes renderiza dentro de un componente padre. Primer argumento es el comp padre y segundo arg es el componente que renderiza dentro del padre (todos los Todo). Esto devuelve un array de componentes renderizados, por lo que tenemos que comprobar su longitud
+
+		expect(todosComponents.length).toBe(todos.length);
+
+	});
+})
