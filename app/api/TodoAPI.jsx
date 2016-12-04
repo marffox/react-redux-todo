185eccdc3a38805f3aadb621c5d09e6efccfe698
Rafael <marffox@gmail.com>
2016-12-04 20:09:42 +0100

    Create filters

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -19,5 +19,34 @@ module.exports = {
 		}
 
 		return $.isArray(todos) ? todos : [];
+	},
+	filterTodos: function(todos, showCompleted, searchText) {
+		var filteredTodos = todos;
+
+		//filter by showCompleted
+		filteredTodos = filteredTodos.filter((todo) => {
+			return !todo.completed || showCompleted;
+		});
+		//filter by searchText
+		filteredTodos = filteredTodos.filter((todo) => {
+			var text = todo.text.toLowerCase();
+			return searchText.length === 0 || text.indexOf(searchText) > -1;//-1: no es encontrado
+		});
+		//sort todos with non-completed first
+		filteredTodos = filteredTodos.sort((a, b) => {
+			/*dados dos valores:
+			si a va antes que b --> return -1
+			si b va antes que a --> return 1
+			si a y b son iguales --> return 0
+			*/
+			if (!a.completed && b.completed) {
+				return -1;
+			} else if (a.completed && !b.completed) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
+		return filteredTodos;
 	}
 }
