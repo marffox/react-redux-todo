d7dc9b1b07b839cb830934c66430517afc3f79d7
marffox <marffox@gmail.com>
2016-12-15 21:05:49 +0100

    Add todo reducers and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -32,4 +32,35 @@ describe('Reducers', () => {
 			expect(res).toEqual(true);
 		});
 	});
+
+	describe('todosReducer', () => {
+		it('Should add new todo', () => {
+			var action = {
+				type: 'ADD_TODO',
+				text: 'walk the dog'
+			};
+			var res = reducers.todosReducer(df([]), df(action));
+
+			expect(res.length).toEqual(1);
+			expect(res[0].text).toEqual(action.text);
+		});
+
+		it('Should toggle todo', () => {
+			var todos = [{
+				id: '22',
+				text: 'do something',
+				completed: true,
+				createdAt: 123,
+				completedAt: 125
+			}];
+			var action = {
+				type: 'TOGGLE_TODO',
+				id: '22'
+			};
+			var res = reducers.todosReducer(df(todos), df(action));
+
+			expect(res[0].completed).toEqual(false);
+			expect(res[0].completedAt).toEqual(undefined);
+		});
+	});
 });
